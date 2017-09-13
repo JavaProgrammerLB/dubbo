df50c23ec42b0fe73c0deba07129e41bc0d6c216
刘备 <admin@yitianyigexiangfa.com>
2017-09-13 14:30:26 +0800

    what is unit test

---
+++ liubei-playground/src/test/java/com/yitianyigexiangfa/dubbo/liubei/playground/test/JunitTest1.java
@@ -0,0 +1,14 @@
+package com.yitianyigexiangfa.dubbo.liubei.playground.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest1 {
+
+    @Test
+    public void onePlusOneTest(){
+        assertEquals(2, 1 + 1);
+    }
+
+}

bcb3e019d627b6a67786784de30e7250cd181d7a
刘备 <admin@yitianyigexiangfa.com>
2017-09-19 16:46:46 +0800

    test

---
+++ liubei-playground/src/test/java/com/yitianyigexiangfa/dubbo/liubei/playground/test/TimeOutTest.java
@@ -0,0 +1,30 @@
+package com.yitianyigexiangfa.dubbo.liubei.playground.test;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class TimeOutTest {
+
+    public static String log;
+    private final CountDownLatch latch = new CountDownLatch(1);
+
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(10);
+
+    @Test
+    public void testSleepForTooLong() throws Exception {
+        log += "ran1";
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    @Test
+    public void testBlockForever() throws Exception{
+        log += "ran2";
+//        latch.await();
+    }
+
+}
